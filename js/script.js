deee934b538a7b3141018ed8550b5f5188f27fd8
vovgreba <49303544+vovgreba@users.noreply.github.com>
2020-04-04 21:05:16 +0300

    finally

--- js/script.js
+++ js/script.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
-  $('a[href^="#"]').click(function () {
-    var elementClick = $(this).attr("href")
-    var destination = $(elementClick).offset().top;
-    jQuery("html:not(:animated),body:not(:animated)").animate({
-      scrollTop: destination
-    }, 1500);
-    return false;
-  });
+	$('a[href^="#"]').click(function () {
+		var elementClick = $(this).attr("href")
+		var destination = $(elementClick).offset().top;
+		jQuery("html:not(:animated),body:not(:animated)").animate({
+			scrollTop: destination
+		}, 1500);
+		return false;
+	});
 
-  $('.menu__btn').click(function () {
+	$(".menu__btn").click(function() {
+		$('.aside__navigation').toggle("slow");  
+	});
 
-    $('.aside__navigation').toggle("slow");
-  })
-});
-
-jQuery(function ($) {
-  $(document).mouseup(function (e) { // событие клика по веб-документу
-    var div = $(".menu__nav"); // тут указываем ID элемента
-    var btn = $(".aside__navigation");
-    if (!div.is(e.target) // если клик был не по нашему блоку
-      &&
-      div.has(e.target).length === 0) { // и не по его дочерним элементам
-      btn.hide("slow"); // скрываем его
-    }
-  });
+	$(document).mouseup(function(e) {
+		var $target = $(e.target);
+		if ($target.closest(".menu__nav").length == 0) {
+			$('.aside__navigation').hide("slow");
+		}
+	});
+	
 });
